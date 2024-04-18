9050685e52f69588b603c5d7e5d7ac618fe8f836
ryokamoi <rfk5465@psu.edu>
2024-04-18 17:50:55 -0400

    added finetuning/tables/train_data_table.txt

--- finetuning/src/generate_tables.py
+++ finetuning/src/generate_tables.py
@@ -10,6 +10,7 @@ metrics_list = ["precision", "recall", "f1"]
 if __name__ == "__main__":
     finetuning_table_dir.mkdir(exist_ok=True, parents=True)
 
+    # table for input type
     table_list: list[str] = []
     for model_name_full in finetuned_models_list:
         table_list.append(model_name_full)
@@ -32,5 +33,32 @@ if __name__ == "__main__":
     
     # save
     with open(finetuning_table_dir / "input_type_table.txt", "w") as f:
+        for line in table_list:
+            f.write(line + "\n")
+
+
+    # table for training data
+    table_list: list[str] = []
+    for model_name_full in finetuned_models_list:
+        table_list.append(model_name_full)
+        model_name = model_name_full.split("/")[-1]
+
+        first_row = [""] + [f"{positive_label} {metrics}" for positive_label in positive_labels_list for metrics in metrics_list]
+        table_list.append(" & ".join(first_row) + " \\\\")
+        for train_data in ["train.csv", "copy_augmentation.csv"]:
+            row_list: list[str] = [train_data]
+            
+            with open(finetuning_results_dir / f"model={model_name},train={train_data},input_format=['user', 'text']/performance.json", "r") as f:
+                performance = json.load(f)["eval_performance"]
+            
+            for positive_label in positive_labels_list:
+                for metrics in ["precision", "recall", "f1"]:
+                    value = performance[positive_label][metrics] * 100
+                    row_list.append(f"{value:.0f}")
+            table_list.append(" & ".join(row_list) + " \\\\")
+        table_list.append("")
+    
+    # save
+    with open(finetuning_table_dir / "train_data_table.txt", "w") as f:
         for line in table_list:
             f.write(line + "\n")
