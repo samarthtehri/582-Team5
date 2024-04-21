444c6a759b764946048c8a38ff0d51ea6a5f7865
ryokamoi <rfk5465@psu.edu>
2024-04-21 11:39:11 -0400

    added label_distribution_by_user_pair to data stats

--- src/get_dataset_stats.py
+++ src/get_dataset_stats.py
@@ -3,6 +3,7 @@ import json
 from src.path import dataset_dir, dataset_stats_dir
 from src.utils.get_performance import get_performance
 from src.utils.dataset_io import load_csv_file_as_list
+from data_augmentation.src.data_augmentation import get_user_pair
 
 
 def get_dataset_stats(data_path: str) -> dict:
@@ -27,6 +28,26 @@ def get_dataset_stats(data_path: str) -> dict:
     
     dataset_stats["baseline_performance"] = simple_baseline_performance
     
+    # user pair distribution
+    user_pair_list = []
+    for row in data:
+        user_pair_list.append(get_user_pair(row))
+    
+    user_pair_distribution = {}
+    for user_pair in user_pair_list:
+        user_pair_distribution[user_pair] = user_pair_distribution.get(user_pair, 0) + 1
+    
+    label_distribution_by_user_pair = {}
+    for i, user_pair in enumerate(user_pair_list):
+        label = labels_list[i]
+        label_distribution_by_user_pair.setdefault(user_pair, {"count": {"0": 0, "1": 0}})
+        label_distribution_by_user_pair[user_pair]["count"][str(label)] += 1
+    
+    dataset_stats["user_pair"] = {
+        "user_pair_distribution": user_pair_distribution,
+        "label_distribution_by_user_pair": label_distribution_by_user_pair,
+    }
+        
     return dataset_stats
 
 
@@ -39,3 +60,12 @@ if __name__ == "__main__":
         dataset_stats_dir.mkdir(exist_ok=True, parents=True)
         with open(dataset_stats_dir / f"{split}.json", "w") as f:
             json.dump(dataset_stats, f, indent=4)
+        
+        # make a table of label_distribution_by_user_pair
+        with open(dataset_stats_dir / f"label_distribution_by_user_pair_{split}.txt", "w") as f:
+            f.write("user_pair & label_0 & label_1 & Num Data\n")
+            for user_pair in ["Alien-Human", "Human-Alien", "Human-Human", "Alien-Alien"]:
+                user_pair_num = dataset_stats["user_pair"]["user_pair_distribution"][user_pair]
+                label_0_percentage = dataset_stats["user_pair"]["label_distribution_by_user_pair"][user_pair]["count"]["0"] / user_pair_num * 100
+                label_1_percentage = dataset_stats["user_pair"]["label_distribution_by_user_pair"][user_pair]["count"]["1"] / user_pair_num * 100
+                f.write(f"{user_pair} & {label_0_percentage:.1f} & {label_1_percentage:.1f} & {user_pair_num}\n")
